6c11faa995c7ed199cf885486896b9cc69f22739
salcrom <seya.28@gmail.com>
2024-12-27 21:01:24 +0100

    bloginfo - Ej 4.5 - paso 3 listo

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,7 +14,24 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+    if (!Array.isArray(blogs) || blogs.length === 0) {
+        return "No hay favoritos";
+    }
+
+    const result = blogs.map(({ title, author, likes }) => ({
+        title,
+        author,
+        likes,
+    }));
+
+    const maxLikes = Math.max(...result.map((blog) => blog.likes));
+
+    return result.find((blog) => blog.likes === maxLikes);
+};
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
