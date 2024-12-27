5b714c4f6e9d24e51ac2e77152be87251e91879e
salcrom <seya.28@gmail.com>
2024-12-27 17:15:02 +0100

    blogunfo - Ej 4.4 - paso 2 listo

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
-    return blogs ? 1 : "No hay blogs";
+    return Array.isArray(blogs) ? 1 : "";
+};
+
+const totalLikes = (blogs) => {
+    if (!Array.isArray(blogs) || blogs.length === 0) {
+        return 0;
+    }
+
+    const reducer = (sum, blog) => {
+        return sum + blog.likes;
+    };
+
+    return blogs.reduce(reducer, 0);
 };
 
 module.exports = {
     dummy,
+    totalLikes,
 };
