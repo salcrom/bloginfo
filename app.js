f20182438134f4e1a3b882b30d3d8622f3e44a74
salcrom <seya.28@gmail.com>
2025-01-08 17:50:32 +0100

    bloginfo E 4.18 - paso 6 listo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const usersRouter = require("./controllers/users");
 const config = require("./utils/config");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
+const loginRouter = require("./controllers/login");
 
 mongoose.set("strictQuery", false);
 
@@ -27,6 +28,7 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 
